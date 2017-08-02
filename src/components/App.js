147c2130818fb59f0e53cf1582ab38387d42bd09
Adrian Rogers <154753+Greatlemer@users.noreply.github.com>
2017-08-02 23:07:48 +0100

    Allow turns to be played
    Add a function for actually playing a turn (that you know you want to
    play), and a function for selecting a random empty space on the board
    and filling it.

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,41 @@ import Board, { newBoard } from './Board';
 import logo from './logo.svg';
 import './App.css';
 
+import { EMPTY_CELL } from './Cell'
+
 class App extends Component {
   constructor() {
     super();
 
+    this.playRandom = this.playRandom.bind(this);
+    this.playTurn = this.playTurn.bind(this);
+
     this.state = {
       board: newBoard(),
     };
+  }
+
+  playRandom(moveId, piece) {
+    const emptyCells = this.state.board.cells.map((cell, index) => {
+      if (cell.contents === EMPTY_CELL) {
+        return index;
+      }
+      return -1;
+    }).filter(index => index > -1);
+    const randomCell = Math.floor(Math.random() * emptyCells.length);
+    this.playTurn(emptyCells[randomCell], moveId, piece);
+  }
+
+  playTurn(location, moveId, piece) {
+    const cells = [
+      ...this.state.board.cells.slice(0, location),
+      {
+        contents: piece,
+        moveId,
+      },
+      ...this.state.board.cells.slice(location + 1),
+    ];
+    this.setState({ board: { ...this.state.board, cells }});
   }
 
   render() {
