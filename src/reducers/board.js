5bd8842f58d7cbf3baa013d8bbe043386e15c7b5
Adrian Rogers <154753+Greatlemer@users.noreply.github.com>
2017-08-06 10:51:49 +0100

    Split out a separate function for reducing the cells

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,15 +1,21 @@
 export default function board(state = {}, action) {
   if (action.type === 'PLAY_TURN') {
     const { location, moveId, piece } = action;
-    const cells = [
-      ...state.cells.slice(0, location),
-      {
-        contents: piece,
-        moveId,
-      },
-      ...state.cells.slice(location + 1),
-    ];
-    return { ...state, cells };
+    return {
+      ...state,
+      cells: updateCellsForTurn(state.cells, location, moveId, piece),
+    };
   }
-  return state
+  return state;
+}
+
+function updateCellsForTurn(cells, location, moveId, piece) {
+  return [
+    ...cells.slice(0, location),
+    {
+      contents: piece,
+      moveId,
+    },
+    ...cells.slice(location + 1),
+  ];
 }
